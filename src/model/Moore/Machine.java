2b6a6fae51d4f15024f76bd9492e7dcb7560b961
Gilmar Amezquita <gilmarandresamezquita@hotmail.com>
2022-10-01 11:52:20 -0500

    Added methods to minimize a moore machine

--- src/model/Moore/Machine.java
+++ src/model/Moore/Machine.java
@@ -7,14 +7,13 @@ import java.util.List;
 public class Machine {
 	public List<String> alphabet;
 	public List<State> mooreStates;
-	public List<State> mooreReducted;
-	
-	public HashMap<String, State> mooreTransitions;
+	public HashMap<String, HashMap<String, State>> mooreTransitions;
 	
 	public Machine(List<String> states, List<String> aStates, List<String> alphabet) {
 		this.alphabet = alphabet;
 		mooreStates = new ArrayList<State>();
 		addStates(states, aStates);
+		addTransitions();
 	}
 	
 	public boolean addStates(List<String> states, List<String> aStates) {
@@ -48,5 +47,78 @@ public class Machine {
 	
 	public void addTransitions() {
 		
+	}
+	
+	public List<List<State>> mooreReduced() {
+		List<State> outFalse = new ArrayList<>();
+		List<State> outTrue = new ArrayList<>();
+		
+		for(State s : mooreStates) {
+			if(s.getStateOut() == 0) {
+				outFalse.add(s);
+			}else outTrue.add(s);
+		}
+		List<List<State>> partitions = new ArrayList<>();
+		partitions.add(outFalse);
+		partitions.add(outTrue);
+		return mooreReduced(partitions);
+	}
+	private List<List<State>> mooreReduced(List<List<State>> partitions){
+		List<List<State>> newPartitions = new ArrayList<>();
+		for(int i = 0; i<partitions.size(); i++) {
+			List<State> p = partitions.get(i);
+			newPartitions.addAll(reducedPartition(p, partitions));
+		}
+		if(partitions.size() != newPartitions.size()) {
+			return mooreReduced(newPartitions);
+		}else return newPartitions;
+	}
+	
+	private List<List<State>> reducedPartition(List<State> p, List<List<State>> parts){
+		List<List<State>> newP = new ArrayList<>();
+		if(p.size()<1) {
+			newP.add(p);
+			return newP;
+		}
+		List<State> p1 = new ArrayList<>();
+		List<State> pPosible = new ArrayList<>();
+		p1.add(p.get(0));
+		for(int i = 1; i<p.size(); i++) {
+			boolean sameTransitionParts = true;
+			for(int j = 0; j<alphabet.size() && sameTransitionParts; j++) {
+				HashMap<String, State> s0 = mooreTransitions.get(p.get(0).getStateName());
+				HashMap<String, State> sa = mooreTransitions.get(p.get(i).getStateName());
+				State s0Transition = s0.get(alphabet.get(j));
+				State saTransition = sa.get(alphabet.get(j));
+				sameTransitionParts = samePreviousPartition(s0Transition, saTransition, parts);
+			}
+			if(sameTransitionParts) {
+				p1.add(p.get(i));
+			}else pPosible.add(p.get(i));
+		}
+		newP.add(p1);
+		if(!pPosible.isEmpty()) {
+			newP.addAll(reducedPartition(pPosible, parts));
+		}
+		return newP;
+	}
+	
+	private boolean samePreviousPartition(State s0, State sa, List<List<State>> parts) {
+		for(List<State> s : parts) {
+			boolean s0Same = false;
+			boolean saSame = false;
+			for(State sS : s) {
+				if(sS.getStateName() == s0.getStateName()) {
+					s0Same = true;
+				}
+				if(sS.getStateName() == sa.getStateName()) {
+					saSame = true;
+				}
+			}
+			if(s0Same && saSame) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
