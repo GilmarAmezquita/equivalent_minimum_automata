5ea3db5e61acec5ab683feb123767de1fee4845d
USUARIO <USUARIO@LAPTOP-GGJUU9B6>
2022-10-02 15:56:30 -0500

    Screen mealy machine added

---
+++ src/ui/MealyMachineController.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MealyMachineController {
+	
+	@FXML
+    private TextField alfabetField;
+
+    @FXML
+    private TextField statesField;
+	
+	public MealyMachineController() {
+		
+	}
+	
+	
+}
--- src/ui/ScreenController.java
+++ src/ui/ScreenController.java
@@ -12,13 +12,32 @@ public class ScreenController {
 	
 	private MooreMachineController mooreController;
 	
+	private MealyMachineController mealyControoler;
+	
 	public ScreenController() {
 		mooreController = new MooreMachineController();
 	}
 	
 	@FXML
     public void selectMealyMachine(ActionEvent event) {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MealyMachineStatesScreen.fxml"));
 		
+		fxmlLoader.setController(mealyControoler);
+		
+		try{
+			Parent root = fxmlLoader.load();
+			
+			Scene scene = new Scene(root);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle("Mealy Machine");
+			stage.setResizable(false);
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
